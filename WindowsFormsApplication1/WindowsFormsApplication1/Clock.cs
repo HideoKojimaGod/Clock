25a4e5ffbb987e209f4da1914215e2ce69579468
Ilya <boss.malinovskiy@mail.ru>
2017-03-31 16:54:15 +0500

    Add files via upload

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Clock.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public class Clock
+    {
+        Timer timer = new Timer();
+        public DateTime Time {get; private set;}
+        public event EventHandler<ClockEventsArgs> Tick;
+        public Clock()
+        {
+            Time = DateTime.Now;
+            timer.Tick += TickHandler;
+            timer.Interval = 1000;
+            timer.Enabled = true;
+            timer.Start();
+            
+        }
+        private void TickHandler(object sender, EventArgs e)
+        {
+            Time = DateTime.Now;
+            var clockEvent = new ClockEventsArgs(Time);
+            OnTick(clockEvent);
+            //Console.WriteLine(c.CurrentDataTime.ToLongTimeString);
+        }
+
+        private void OnTick(ClockEventsArgs e)
+        {
+            var handler = this.Tick;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+        //public void Start()
+        //{
+        //    timer.Start();
+        //    clockTime = DateTime.Now;
+            
+        //}
+    }
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var clock = new Clock();
+            //Thread.Sleep(5000);
+            var form = new ClockForm(clock);
+            Application.Run(form);
+           
+
+            
+        }
+    }
+}
